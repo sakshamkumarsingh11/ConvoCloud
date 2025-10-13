521dcef75dd64c5d7668d797c9e7a9cb0858108f
Saksham Kumar Singh <priti.saksham.ips@gmail.com>
2025-10-13 17:11:33 +0530

    Changed chat title from Peer 2 Peer to ConvoCloud Chat

--- mern-chat-service/backend/src/index.js
+++ mern-chat-service/backend/src/index.js
@@ -2,16 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
+dotenv.config(); // loaded env var here
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
